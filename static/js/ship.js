6ae3c69adc1d862ec5de911284652c90b5f0677f
Gigo <gigo@muc.ccc.de>
2018-04-04 07:38:07 +0200

    static function toRelMobility() seperated

--- static/js/ship.js
+++ static/js/ship.js
@@ -1,37 +1,3 @@
-var shipconfig= {
-	assets : {},
-
-	xwing : {
-		gltf:"gltf/xwing.gltf",
-		gltfScale:0.06,
-		gltfRot:{x:0, y:180, z:0},
-
-		mobility: {
-				minSpeed:0.4,
-				maxSpeed:1.3,
-				maxYaw:80,
-				maxPitch:70,
-				maxRoll:45
-			},
-	},
-	tie : {
-		gltf:"gltf/tie.gltf",
-		gltfScale:0.135,
-		gltfRot:{x:0, y:180, z:0},
-
-		mobility: {
-				minSpeed:0.2,
-				maxSpeed:0.8,
-				maxYaw:110,
-				maxPitch:80,
-				maxRoll:45
-			},
-
-	}
-};
-
-
-
 AFRAME.registerComponent('ship', {
 	dependencies:['statussphere'],
 	
@@ -42,42 +8,13 @@ AFRAME.registerComponent('ship', {
 	},
 
 	init:function(){ 
-	  
-	  //TODO: this function should be accessable from front- and backend...
-		this.toRel=function(pos, rot, sel, rel) {
-      //console.log(pos, rot, sel, rel);
-
-			var minSpeed = this.config.mobility.minSpeed;
-			var maxSpeed = this.config.mobility.maxSpeed;
-	    	var maxYaw = this.config.mobility.maxYaw;
-	    	var maxPitch = this.config.mobility.maxPitch;
-	    	var maxRoll = this.config.mobility.maxRoll;
-
-	        var newPitch = rot.x - sel.pitch*maxPitch*rel*rel*2;
-			var dPitch = (newPitch+rot.x)/2;
-
-			var newYaw = rot.y - sel.yaw*maxYaw*rel*rel*2;
-			var dYaw = (newYaw+rot.y)/2;
-
-			var newRoll = rot.z*(1-rel*rel) - sel.yaw*maxRoll*rel*rel;
-
-			var speed = (sel.throttle*(maxSpeed-minSpeed)+minSpeed)*rel;
-
-	        var dx = -Math.cos(dPitch*Math.PI/180)* Math.sin(dYaw*Math.PI/180)*speed;
-	    	var dy = Math.sin(dPitch*Math.PI/180)*speed;
-	    	var dz = -Math.cos(dPitch*Math.PI/180)*Math.cos(dYaw*Math.PI/180)*speed;
-
-	        var posXNew = pos.x+dx;
-	        var posYNew = pos.y+dy;
-	        var posZNew = pos.z+dz;
-
-	        return {x:posXNew, y:posYNew, z:posZNew, pitch: newPitch, yaw:newYaw, roll:newRoll};
-		};
-
 		this.to=function(pos, rot, sel) {
 			return this.toRel(pos,rot,sel,1);
 		}
-
+	  
+		this.toRel=function(pos, rot, sel, rel) {
+      return shipconfig.toRelMobility(pos, rot, sel, rel, this.config.mobility);
+		};
 
 		this.doesAssetExist = function() {
 			return (shipconfig.assets[this.type]!=undefined);
